345b1eb62498b4680ff9fcf7d4d1333196956689
shashika05 <shashikaraveenyasas@gmail.com>
2021-07-21 14:48:17 +0530

    Added temporary data

---
+++ tempData.js
@@ -0,0 +1,73 @@
+const tempData = [
+  {
+    id: 1,
+    name: "Plan a trip",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Book Flight",
+        completed: false,
+      },
+      {
+        title: "Passport Check",
+        completed: false,
+      },
+      {
+        title: "Reserve Hotel Room",
+        completed: true,
+      },
+      {
+        title: "Pack Luggage",
+        completed: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Weekend todos",
+    color: "#8022D9",
+    todos: [
+      {
+        title: "Buy Milk",
+        completed: false,
+      },
+      {
+        title: "Buy Tickets",
+        completed: true,
+      },
+      {
+        title: "Reserve seats for bus",
+        completed: false,
+      },
+      {
+        title: "Shopping",
+        completed: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Build a PC",
+    color: "#d9b722",
+    todos: [
+      {
+        title: "Buy CPU",
+        completed: false,
+      },
+      {
+        title: "Buy GPU",
+        completed: true,
+      },
+      {
+        title: "Buy a casing with fans",
+        completed: false,
+      },
+      {
+        title: "Buy other meterials",
+        completed: true,
+      },
+    ],
+  },
+];
+
+export default tempData;
