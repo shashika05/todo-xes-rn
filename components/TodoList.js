e76ac3432a0f108a9a19c5d8470927e7e283bdbd
shashika05 <shashikaraveenyasas@gmail.com>
2021-07-21 15:04:29 +0530

    Updated TodoList.js

---
+++ components/TodoList.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { Modal, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import colors from "../Colors";
+
+import TodoModal from "./TodoModal";
+
+export default class TodoList extends React.Component {
+  state = {
+    showListVisible: false,
+  };
+
+  toggleListModal() {
+    this.setState({ showListVisible: !this.state.showListVisible });
+  }
+
+  render() {
+    const list = this.props.list;
+
+    const completedCount = list.todos.filter((todo) => todo.completed).length;
+    const remainingCount = list.todos.length - completedCount;
+
+    return (
+      <View>
+        <Modal
+          animationType="slide"
+          visible={this.state.showListVisible}
+          onRequestClose={() => this.toggleListModal()}
+        >
+          <TodoModal
+            list={list}
+            closeModel={() => this.toggleListModal()}
+            updateList={this.props.updateList}
+          />
+        </Modal>
+        <TouchableOpacity
+          style={[styles.listContainer, { backgroundColor: list.color }]}
+          onPress={() => this.toggleListModal()}
+          onLongPress={() => this.props.deleteList(list)}
+        >
+          <Text style={styles.listTitile} numberOfLines={1}>
+            {list.name}
+          </Text>
+          <View>
+            <View style={{ alignItems: "center" }}>
+              <Text style={styles.count}>{remainingCount}</Text>
+              <Text style={styles.subtitle}>Remaining</Text>
+            </View>
+            <View style={{ alignItems: "center" }}>
+              <Text style={styles.count}>{completedCount}</Text>
+              <Text style={styles.subtitle}>Completed</Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  listContainer: {
+    paddingVertical: 32,
+    paddingHorizontal: 16,
+    borderRadius: 6,
+    marginHorizontal: 12,
+    alignItems: "center",
+    width: 200,
+  },
+  listTitile: {
+    fontSize: 24,
+    fontWeight: "700",
+    color: colors.white,
+    marginBottom: 18,
+  },
+  count: {
+    fontSize: 48,
+    color: colors.white,
+    fontWeight: "200",
+  },
+  subtitle: {
+    fontSize: 12,
+    fontWeight: "700",
+    color: colors.white,
+  },
+});
